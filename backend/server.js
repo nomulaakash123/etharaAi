bf91ef86fc3278f4da167a06b690ddd3e3c2393e
nomulaakash123 <145181682+nomulaakash123@users.noreply.github.com>
2026-05-02 18:56:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://innovative-delight-production.up.railway.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 app.use("/users", require("./routes/userRoutes"));
